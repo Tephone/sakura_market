1d9847bf69f95569468212854edf6affd23dbcf5
naoko kasahara <kassa0918@gmail.com>
2021-06-10 15:43:33 +0900

    admin_users_veiw作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product.delete
+    @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
       format.json { head :no_content }
