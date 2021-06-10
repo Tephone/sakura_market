775fb0c31a4508ca622acae3ccf3ceb566f1c61d
naoko kasahara <kassa0918@gmail.com>
2021-06-10 17:50:45 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
-  end
-  namespace :admin do
-    resources :products
-  end
   root 'products#index'
   resources :products 
     namespace :admin do
       resources :products
     end
-    namespace :admin do
-      resources :users
-    end
+
   resources :orders, only: [:index, :new, :create]
   devise_for :users
+  namespace :admin do
+    resources :users
+  end
 end
